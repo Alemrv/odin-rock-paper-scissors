594d3faa1c7aad4c5b89b739c3df3f4f6743a0d9
Alemrv <alemrv@gmail.com>
2021-09-20 22:08:22 -0400

    -added style file -changed the logic of the game, you can't tie now and you have to play until someone wins 5 games

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let playerScore = 0;
 let computerScore = 0;
-let round = 0;
 
 const roundResult = document.querySelector(".roundResult");
-const score = document.querySelector(".score");
+const pcScoreDiv = document.querySelector(".pc-score-div");
+const playerScoreDiv = document.querySelector(".player-score-div");
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const btnHide = document.querySelector(".btn-hide");
 
 function computerPlay(){
     let computerAnswers = ["rock", "paper", "scissors"];
@@ -15,48 +19,41 @@ function playRound(playerSelection, computerSelection){
 
     switch (true) {
         case playerSelection == "rock" && computerSelection == "paper":
-            roundResult.textContent = "you lose, paper beats rock";
+            roundResult.textContent = "you lose! paper beats rock";
             computerScore++;
-            round++;
             break;
     
         case playerSelection == "paper" && computerSelection == "rock":
-            roundResult.textContent = "you win!!, paper beats rock";
+            roundResult.textContent = "you win! paper beats rock";
             playerScore++;
-            round++;
             break;
 
         case playerSelection == "rock" && computerSelection == "scissors":
-            roundResult.textContent = "you win!!, rock beats scissors";
+            roundResult.textContent = "you win! rock beats scissors";
             playerScore++;
-            round++;
             break;
         
         case playerSelection == "scissors" && computerSelection == "rock":
-            roundResult.textContent = "you lose, rock beats scissors";
+            roundResult.textContent = "you lose! rock beats scissors";
             computerScore++;
-            round++;
             break;
 
         case playerSelection == "paper" && computerSelection == "scissors":
-            roundResult.textContent = "you lose, scissors beats paper";
+            roundResult.textContent = "you lose! scissors beats paper";
             computerScore++;
-            round++;
             break;
             
         case playerSelection == "scissors" && computerSelection == "paper":
-            roundResult.textContent = "you win!!, scissors beats paper";
+            roundResult.textContent = "you win! scissors beats paper";
             playerScore++;
-            round++;
             break;
         default:
-            round++;
             roundResult.textContent = "It's a draw!";
                
     }
 }
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
 
@@ -67,21 +64,35 @@ buttons.forEach((button) => {
 
         playRound(playerSelection, computerSelection);
 
-        score.textContent = `Round ${round} the score is: ${playerScore} for the player and ${computerScore} for the computer`;
-        if (round === 5) {
+        pcScoreDiv.textContent = `${playerScore}`;
+        playerScoreDiv.textContent = `${computerScore}`;
+
+        if (playerScore === 5 || computerScore === 5) {
             if (playerScore > computerScore) {
                 roundResult.textContent = "YOU WON THE GAME!!!!";
-            }
-            else if(playerScore === computerScore){
-                roundResult.textContent = "IT'S A TIE!";
+
             }
             else{
                 roundResult.textContent = "YOU LOST THE GAME!!!!";
+                
             }
-            score.textContent = `Final Round ${round} the score is: ${playerScore} for the player and ${computerScore} for the computer`;
-            round = 0;
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
+            btnHide.style.display = "block";
             playerScore = 0;
             computerScore = 0;
+            return;
         }      
     });
 });
+
+btnHide.addEventListener("click", ()=>{
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+    btnHide.style.display = "none";
+    pcScoreDiv.textContent = `${playerScore}`;
+    playerScoreDiv.textContent = `${computerScore}`;
+    roundResult.textContent = "";
+})
